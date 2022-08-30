03891017b9757fcb57bf808c513dcebba84a0002
Senak9 <joaomarcerlo.o.rocha@gmai.com>
2022-08-30 01:43:07 -0300

    feat: integração página de cadastro

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -43,7 +43,7 @@ export default function Login() {
   }
 
   return (
-    <div className="containerLogin">
+    <div className="containerLogin" onSubmit={handleSubmit}>
       <Header />
       <input
         name="Username"
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,17 +1,60 @@
 import React from "react";
 import Header from "../../component/header";
 import "./style.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const defaultForm = {
+  name: "",
+  password: "",
+  confirmPassword: "",
+};
 
 export default function Register() {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({ ...defaultForm });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      if (!form.name || !form.password || !form.confirmPassword) {
+        return;
+      }
+
+      if (form.password !== form.confirmPassword) {
+        return;
+      }
+
+      const response = await api.post("/usuario", {
+        nome: form.name,
+        senha: form.password,
+      });
+
+      if (response.status > 204) {
+        return;
+      }
+
+      navigate("/");
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  function handleChangeForm({ target }) {
+    setForm({ ...form, [target.name]: target.value });
+  }
+
   return (
-    <div className="containerRegister">
+    <div className="containerRegister" onSubmit={handleSubmit}>
       <Header />
       <input
         name="Username"
         placeholder="Username"
         type="text"
-        // value={}
-        // onChange={(e) => setUsername(e.target.value)}
+        value={form.name}
+        onChange={handleChangeForm}
         // required
       />
       <input
@@ -19,16 +62,16 @@ export default function Register() {
         placeholder="Senha"
         type="password"
         className="inputSenha"
-        // value={password}
-        // onChange={(e) => setPassword(e.target.value)}
+        value={form.password}
+        onChange={handleChangeForm}
         // required
       />
       <input
         name="Senha"
         placeholder="Confimar senha"
         type="password"
-        // value={password}
-        // onChange={(e) => setPassword(e.target.value)}
+        value={form.password}
+        onChange={handleChangeForm}
         // required
       />
       <button className="btn-register">Cadastrar</button>
