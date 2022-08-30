6be607839bf0154682037c7e49ad56a6738c8106
Senak9 <joaomarcerlo.o.rocha@gmai.com>
2022-08-30 01:21:32 -0300

    feat: integração página de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import Header from "../../component/header";
 import "./style.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [nome, setNome] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const token = getItem("token");
+
+    if (token) {
+      navigate("/home");
+    }
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      if (!nome || !password) {
+        return;
+      }
+
+      const response = await api.post("/login", {
+        nome,
+        senha: password,
+      });
+
+      const { usuario, token } = response.data;
+
+      setItem("token", token);
+      setItem("userId", usuario.id);
+      setItem("userName", usuario.nome);
+
+      navigate("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="containerLogin">
       <Header />
@@ -11,17 +50,15 @@ export default function Login() {
         placeholder="Username"
         type="text"
         className="inputText"
-        // value={}
-        // onChange={(e) => setUsername(e.target.value)}
-        // required
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
       />
       <input
         name="Senha"
         placeholder="Senha"
         type="password"
-        // value={password}
-        // onChange={(e) => setPassword(e.target.value)}
-        // required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <button className="btn-login">Enviar</button>
     </div>
